6bfc6ac62a60ac698f167acdd3513783573a6bac
Aryan Suryawanshi <aryanvgsurya@gmail.com>
2026-07-19 14:52:14 +0530

    Configured Brevo SMTP

--- backend/settings.py
+++ backend/settings.py
@@ -136,11 +136,11 @@ CORS_ALLOWED_ORIGINS = [
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 
-EMAIL_PORT = 587
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 
